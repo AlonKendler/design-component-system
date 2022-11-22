97f3fd4dff05d15ff0fb034298d50c7f1fc59bf8
alon kendler <alonkendler@gmail.com>
2022-11-22 13:44:21 +0200

    update app.tsx to work, should wrap with parent?

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
+import SelectMulti from "./components/SelectMulti";
+import SelectSingle from "./components/SelectSingle";
 
 
-import SelectContainer from "./components/SelectContainer";
 
-const args = {
+
+const props = {
 
   label: "select component",
   placeholder: "select...",
-  multi: true 
+
+  options: [
+    { id: 0, label: "red", isSelected: false },
+    { id: 1, label: "blue", isSelected: false },
+    { id: 2, label: "purple", isSelected: true },
+    { id: 3, label: "yellow", isSelected: false },
+    { id: 5, label: "brown", isSelected: false },
+    { id: 6, label: "lade", isSelected: false },
+    { id: 7, label: "gold", isSelected: false },
+  ]
 };
 
-const options = [
-  { id: 0, label: "red", isSelected: false },
-  { id: 1, label: "blue", isSelected: false },
-  { id: 2, label: "purple", isSelected: true },
-  { id: 3, label: "yellow", isSelected: false },
-  { id: 5, label: "brown", isSelected: false },
-  { id: 6, label: "lade", isSelected: false },
-  { id: 7, label: "gold", isSelected: false },
-]
 
 function App() {
 
   return (
     <>
-      <SelectContainer optionsList={options} {...args} />
+      <SelectSingle {...props} />
+      <SelectMulti {...props} />
     </>
   );
 }
